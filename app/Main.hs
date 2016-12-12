dcac6edb38a1568ac91cd1a13cee016000c6f5a9
Vanessa McHale <tmchale@wisc.edu>
2016-12-12 12:28:49 -0600

    initial commit

---
+++ app/Main.hs
@@ -0,0 +1,11 @@
+{-#LANGUAGE OverloadedStrings #-}
+
+import Data.QRCodes
+import qualified Data.ByteString as B
+import System.Environment (getArgs)
+
+main :: IO ()
+main = do
+    pipeIn <- B.getContents
+    filepath <- fmap (flip (!!) 0) getArgs
+    byteStringToQRSec pipeIn filepath
